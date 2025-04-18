12e2ee31058469e70060e14193cdb5e301178c9c
mohanasundharam <mohanasundharamms271@gmail.com>
2025-04-18 13:01:28 +0000

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,38 @@
+import {IoCart} from 'react-icons/io5'
+import {Link} from 'react-router-dom'
+import CartContext from '../../CartContext'
+import './index.css'
+
+const Header = props => {
+  const {restaurantName} = props
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+
+        const getCartCount = cartList.reduce(
+          (sum, each) => each.quantity + sum,
+          0,
+        )
+
+        return (
+          <nav className="navbar px-4">
+            <Link to="/">
+              <h1 className="nav-head">{restaurantName}</h1>
+            </Link>
+            <Link to="/cart" className="cart-icon">
+              <p className="d-none d-md-block mt-3">My Orders</p>
+
+              <button type="button" data-testid="cart" className="nav-cart">
+                <IoCart size={23} />
+                <p className="count">{getCartCount}</p>
+              </button>
+            </Link>
+          </nav>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
+
+export default Header
